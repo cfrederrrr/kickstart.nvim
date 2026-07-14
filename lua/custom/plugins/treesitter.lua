38aa1e51dde03a8c950a0370f7973b37ba21078a
dusty <galvertez@gmail.com>
2026-07-14 00:34:42 -0400

    backup

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -35,6 +35,17 @@ return {
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
+    config = function(_, opts)
+      require('nvim-treesitter').setup(opts)
+      -- nvim-treesitter main branch no longer manages highlighting itself;
+      -- Neovim only auto-starts treesitter for its own bundled parsers.
+      -- This autocmd covers third-party parsers like zig.
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(ev)
+          pcall(vim.treesitter.start, ev.buf)
+        end,
+      })
+    end,
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
